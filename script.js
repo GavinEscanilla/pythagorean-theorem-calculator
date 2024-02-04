4c6183c8950cafd829c6719436674d84b1aad588
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-02-04 04:49:02 -0500

    finished all asnwers but my output stops working after a few clicks

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ const calculate = () => {
     const b = input2.value;
     const c = input3.value;
  if( a > 0 && b > 0 && c === ''){
+   if(a > b){
     let calc = Math.sqrt(a * a + b * b);
     answer.textContent = 'answer: ' +calc;
- } else if( a > 0 && b === '' && c > 0){
-    let calc = Math.sqrt(a * a - c * c);
+ }else{
+   let calc = Math.sqrt(b * b + a * a);
     answer.textContent = 'answer: ' +calc;
  }
+ } else if( a > 0 && b === '' && c > 0){
+   if(c > a){
+    let calc = Math.sqrt(c *c - a * a);
+    answer.textContent = 'answer: ' +calc;
+   }else{
+      let calc = Math.sqrt(a *a - c * c);
+      answer.textContent = 'answer: ' +calc;
+   }
+ }else if( a === '' && b > 0 && c > 0){
+   if(c > b){
+      let calc = Math.sqrt(c *c - b * b);
+      answer.textContent = 'answer: ' +calc;
+     }else{
+        let calc = Math.sqrt(b *b - c * c);
+        answer.textContent = 'answer: ' +calc;
+     }
+ }else if( a=== '' && b === '' && c === ''){
+   answer.textContent = 'Please enter number';
+ }
+ 
     }
